97f8b134b940186b4450ee3e0b292c4551bf4ecd
Ella Costello <ella.costello@yahoo.com>
2025-11-17 11:29:38 +0100

    Initial project setup with added configurations
    - Spring web
    - MySQL
    - Thymeleaf

---
+++ src/main/java/com/hauxy/projectwizard/ProjectWizardApplication.java
@@ -0,0 +1,13 @@
+package com.hauxy.projectwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWizardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWizardApplication.class, args);
+    }
+
+}
