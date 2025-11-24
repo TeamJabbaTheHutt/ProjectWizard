bb29b8bb99f1c9e04085541410491c0a0d2c4ece
Theis Milling <theismillinger@gmail.com>
2025-11-24 13:52:06 +0100

    started login, made a trial for html without css
    logincontroller
    loginservice

--- src/main/java/com/hauxy/projectwizard/controller/LoginController.java
+++ src/main/java/com/hauxy/projectwizard/controller/LoginController.java
@@ -1,9 +1,15 @@
 package com.hauxy.projectwizard.controller;
 
+import com.hauxy.projectwizard.model.User;
 import com.hauxy.projectwizard.service.LoginService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/login")
 @Controller
@@ -13,4 +19,26 @@ public class LoginController {
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
     }
+
+    @GetMapping()
+    public String showLogin() {
+        return "login";
+    }
+
+    @PostMapping()
+    public String handleLogin(@RequestParam String email, @RequestParam String password, Model model, HttpSession httpSession) {
+        User user = loginService.checkCredentials(email, password);
+
+        if (user != null) {
+            model.addAttribute("message", "Login successful");
+            model.addAttribute("messageType", "success");
+            httpSession.setAttribute("loggedInUser", user);
+            return "redirect:/dashboard";
+        } else {
+            model.addAttribute("message", "Invalid username or password");
+            model.addAttribute("messageType", "error");
+            return "login";
+        }
+    }
+
 }
--- src/main/java/com/hauxy/projectwizard/service/LoginService.java
+++ src/main/java/com/hauxy/projectwizard/service/LoginService.java
@@ -1,5 +1,6 @@
 package com.hauxy.projectwizard.service;
 
+import com.hauxy.projectwizard.model.User;
 import com.hauxy.projectwizard.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,16 @@ public class LoginService {
 
     public LoginService(UserRepository userRepository) {
         this.userRepository = userRepository;
+
     }
+
+    public User checkCredentials(String email, String password) {
+        User user = userRepository.getUserByEmail(email);
+
+        if (user == null || !user.getPassword().equals(password)) {
+            return null;
+        }
+        return user;
+    }
+
 }
